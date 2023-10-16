5363c30e7e0c78419ab7a3065885f0ef078eb8d7
cBombs <richie.ucsb@gmail.com>
2023-10-16 15:50:56 -0700

    hardhat tests, beforeEach(), describe, async

---
+++ test/Counter.js
@@ -0,0 +1,21 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Counter", () => {
+  let counter;
+
+  beforeEach(async () => {
+    const Counter = await ethers.getContractFactory("Counter");
+    counter = await Counter.deploy("My Counter", 1);
+  });
+
+  describe("Deployment", () => {
+    it("sets the initial count", async () => {
+      expect(await counter.count()).to.equal(1);
+    });
+
+    it("sets the initial name", async () => {
+      expect(await counter.name()).to.equal("My Counter");
+    });
+  });
+});
